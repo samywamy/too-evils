680b6ed512258dae69f27d91701c1de71c358317
samywamy <sam.alexis.ng@gmail.com>
2018-08-07 23:30:52 +0800

    before action auth user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, :except => [ :show, :index ]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
