3c724484d7b030eeeffc1affa01ae922fb3826a7
samywamy <sam.alexis.ng@gmail.com>
2018-08-14 00:56:13 +0800

    routes.rb rearrange

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
-	
+
 	def index
 		@q = Question.first
-		# @q = Question.new
-		# @q.answer_1 = "pee"
-		# @q.answer_2 = "hairball"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
+  get '/questions/vote', to: 'votes#upvote_questions'
+
   resources :questions
+
+  devise_for :users
 
   resources :users do
     resources :questions
   end
 
   get '/vote/:question_id/:voted_answer', to: 'votes#register_vote'
-
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/vote/:question_id', to: 'votes#register_vote'
+  
 end
