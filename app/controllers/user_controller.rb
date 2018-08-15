8161084ae47e1eb4856524b8cd366ba3bb4a218e
samywamy <sam.alexis.ng@gmail.com>
2018-08-15 20:48:26 +0800

    sort user questions by total votes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,7 @@ class UserController < ApplicationController
 
     def profile
         @username = User.where(id: current_user.id).first.email
-        @questions = Question.where(user_id: current_user.id)
+        @questions = Question.where(user_id: current_user.id).sort { |q2, q1| (q1.votes_answer_1 + q1.votes_answer_2) <=> (q2.votes_answer_1 + q2.votes_answer_2) }
         @question = Question.new
     end
 
